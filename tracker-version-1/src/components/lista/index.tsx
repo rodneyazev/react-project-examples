444e75381dfdfe6987accb01bc2985e576b71138
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-15 22:05:10 -0300

    First React project example

---
+++ tracker-version-1/src/components/lista/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Lista(){
+    return (
+        <div>
+            <aside>
+                            <h2>Estudos do dia</h2>
+            </aside>
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>02:00:00</span>
+                </li>
+                <li>
+                    <h3>JavaScript</h3>
+                    <span>01:00:00</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Lista;
