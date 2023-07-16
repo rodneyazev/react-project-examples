0e1830d56637b676719b4605b1c3acc77190bd46
rodneyazev <rodney.n.azevedo@gmail.com>
2023-07-15 23:53:49 -0300

    Stylin SCSS

---
+++ tracker-version-3/src/components/lista/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from './Lista.module.scss';
+
+function Lista(){
+
+    const tarefas = [
+        {tarefa: 'React', tempo: '02:00:00'},
+        {tarefa: 'JavaScript', tempo: '01:00:00'},
+        {tarefa: 'TypeScript', tempo: '03:00:00'}
+    ]
+
+    return (
+        <div>
+            <aside className={style.listaTarefas}>
+                <h2>Estudos do dia</h2>
+            </aside>
+            <ul>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index} className={style.item}>
+                        <h3>{tarefa.tarefa}</h3>
+                        <span>{tarefa.tempo}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
+
+export default Lista;
---
+++ tracker-version-3/src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Formulario from '../components/formulario';
+import Lista from '../components/lista';
+import './style.scss';
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
+
+export default App;
